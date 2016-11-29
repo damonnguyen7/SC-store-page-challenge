d06d29dd0094cdff88f0b4542c0fb5b1af431106
Damon <damonnguyen92@gmail.com>
2016-11-29 11:30:38 -0800

    Feat: Require modules path, body-parser, express-ejs-layout, set up template engine, and middleware for EL, router, bodyParser static pages

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-//Require express
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+var router = require('./app/routes');
 var port = process.env.PORT || 3000;
-//Create express app
 var app = express();
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+//Middleware:
+app.use(expressLayouts); //override the default response.render() behavior
+app.use('/', router); 
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
 //Boot up server
 app.listen(port, function() {
   console.log('App listening on: ' + port);
