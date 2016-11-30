f5348a450b020bb5cd9a9cbf25ea506da0cd2adc
Damon <damonnguyen92@gmail.com>
2016-11-30 11:28:16 -0800

    Feat: Create home and store request handler that temporary console.log

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -1,5 +1,11 @@
 module.exports = {
   landingPage: function(req, res) {
     res.render('layout');
+  },
+  home: function(req, res) {
+    console.log('at home page!');
+  },
+  store: function(req, res) {
+    console.log('at store page!')
   }
 }
