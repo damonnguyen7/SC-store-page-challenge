8d0f627c83135a56dd1b6f70d75d762ec2421e81
Damon <damonnguyen92@gmail.com>
2016-11-30 11:29:23 -0800

    Feat(init): Create file for routing for the backend

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,14 @@ var router = express.Router();
 var controller = require('./controller/mainController');
 
 var landingPage = controller.landingPage;
+var home = controller.home;
+var store = controller.store;
+
 
 //GET Request:
 router.get('/', landingPage);
+router.get('/#/home', home);
+router.get('/#/store', store);
 
 //Export router for server to use in middleware: 
 module.exports = router;
