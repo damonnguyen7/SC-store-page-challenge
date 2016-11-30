3c68ae53f19e82c5564871fc57c3c9304d8acf80
Damon <damonnguyen92@gmail.com>
2016-11-30 13:42:46 -0800

    Refactor: Remove request handler logic for home and store

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -1,11 +1,5 @@
 module.exports = {
   landingPage: function(req, res) {
     res.render('layout');
-  },
-  home: function(req, res) {
-    console.log('at home page!');
-  },
-  store: function(req, res) {
-    console.log('at store page!')
   }
 }
